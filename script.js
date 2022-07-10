5b0ff55d8d447e46b5617c59d243ecd5ef110d28
dpablo2018 <104723936+dpablo2018@users.noreply.github.com>
2022-07-10 10:56:39 -0700

    Add files via upload

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
-fetch ('https://www.balldontlie.io/api/v1/players')
+fetch ('https://www.balldontlie.io/api/v1/players') // Fetches the api
 
-    .then(response => response.json())
-    .then(data => {
-        console.log(data)
-        console.log(data.data[0])
-        console.log(data.data[11].height_feet)
+    .then(response => response.json()) // We call to the object response to get the data 
+    .then(data => { //
+        console.log(data) // Print data to console 
+        console.log(data.data[0]) // The .data goes down the array and goes to the 0 element of the array 
+        console.log(data.data[11].height_feet) // The .data goes down the array to the 11th element and goes down  a different array again to the data about height feet
     });
 
 
@@ -13,3 +13,4 @@ let person = "Kelvin";
 var people = [person, "George", "Elmer"];
 
 var food = {favorite: "tacos", american: "burgers", mexican: "Tortas"}
+
